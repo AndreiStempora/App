8f21f58d6008d6e18fbd40ec708f9cf70445aa20
Andrei <andrei@Emanuels-Mac-mini.local>
2023-02-14 18:29:48 +0200

    dc am venit azi?

--- src/pages/vehicles/hotspotType/components/camera/OpenedCameraTakePhoto.jsx
+++ src/pages/vehicles/hotspotType/components/camera/OpenedCameraTakePhoto.jsx
@@ -1,6 +1,5 @@
 import { IonButtons, IonFooter, IonGrid, IonRow, IonButton, IonIcon, IonContent, IonLabel, IonImg } from "@ionic/react";
 import { useRSelection } from "../../../../../packages/database/features/utils/utilityHooks";
-import { useEffect } from "react";
 import { CustomContent, CustomHeader } from "../../../../../components/page/Page";
 import HotspotSwiper from "./HotspotSwiper";
 import useCamera from "../../../../../packages/camera/features/CameraCustomHook";
